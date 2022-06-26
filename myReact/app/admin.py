f99bd8feba6c135aad529acd35294e559a49dccd
Abshishek <jha98451@gmail.com>
2022-06-26 21:04:37 +0530

    Done
    Done

--- myReact/app/admin.py
+++ myReact/app/admin.py
@@ -1,3 +1,9 @@
+from pydoc import plain
 from django.contrib import admin
-
+from .models import Plan
 # Register your models here.
+
+
+@admin.register(plain)
+class PlanAdmin(admin.ModelAdmin):
+    list_display = ['id', 'item']
